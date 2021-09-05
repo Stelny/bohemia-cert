e87a009c5fd6a9b2d8fa47b67b5b0200248ec7e0
Jakub Štellner <57998476+Stelny@users.noreply.github.com>
2021-09-05 22:58:06 +0200

    Šablona

--- js/script.js
+++ js/script.js
@@ -13,25 +13,68 @@ $(document).ready(function(){
       dots:false,
       responsive:{
           0:{
-              items:1
+              items:2
           },
           500:{
+            items:3
+          },
+          776:{
             items:4
           },
           991:{
+            items:5
+          },
+          1200:{
               items:6
           },
       }
   })
 
-  $('.partners-ar.right').click(function(e) {
+  $('.partners .nav-circle.right').click(function(e) {
     e.preventDefault();
     partners.trigger('next.owl.carousel');
     })
 
-    $('.partners-ar.left').click(function(e) {
+    $('.partners .nav-circle.left').click(function(e) {
         e.preventDefault();
         partners.trigger('prev.owl.carousel');
+    })
+
+
+    var learn = $('.learning__carousel');
+
+    learn.owlCarousel({
+      loop:true,
+      margin:10,
+      nav:false,
+      dots:false,
+      responsive:{
+          0:{
+              items:2
+          },
+          500:{
+            items:3
+          },
+          776:{
+            items:3
+          },
+          991:{
+            items:4
+          },
+          1200:{
+              items:5
+          },
+      }
+  })
+
+  $('.learn .nav-circle.right').click(function(e) {
+    e.preventDefault();
+    learn.trigger('next.owl.carousel');
+    })
+
+    $('.learn .nav-circle.left').click(function(e) {
+        e.preventDefault();
+        learn.trigger('prev.owl.carousel');
     })
 
 
